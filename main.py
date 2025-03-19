8a59f1baa22182358f04a5dd2e615fb77db6f928
Lulan <Lulan1@web.de>
2025-03-19 22:05:30 +0100

    Sortieren läuft

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ def get_book_text(file_path):
     return inhalt
 
 from stats import word_count
+from stats import letter_count
+from stats import sorted_list
 
 file_path = "books/frankenstein.txt"
 book_text = get_book_text(file_path)
 anzahl = word_count(book_text)
+menge = letter_count(book_text)
+sortiert = sorted_list(menge)
 
-print(f"{anzahl} words found in the document")
+print("============ BOOKBOT ============")
+print("Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
+print(f"Found {anzahl} total words")
+print("--------- Character Count -------")
+for line in sortiert:
+    print(line)
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -1,3 +1,19 @@
 def word_count(book_text):
     anzahl = len(book_text.split())
     return anzahl 
+
+def letter_count(book_text):
+    letters = {} 
+    for buchstabe in book_text.lower():
+        if buchstabe in letters:
+            letters[buchstabe] += 1
+        else:
+            letters[buchstabe] =  1
+    return letters
+
+def sorted_list(menge):
+    formatted_list = [
+    f"{buchstabe}: {anzahl}" 
+    for buchstabe, anzahl in sorted(menge.items(), key=lambda x: x[1], reverse=True)
+]
+    return [line for line in formatted_list if line[0].isalpha()]
